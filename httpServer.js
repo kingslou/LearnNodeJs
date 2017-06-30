a999f2665604a6c34d5f15b4a2a111b133650f5a
kingslou <admin123456>
2017-06-30 14:34:31 +0800

    通过 require 应用外部js

--- httpServer.js
+++ httpServer.js
@@ -1,17 +1,17 @@
 var http = require('http');
-
+var version = require('./model/version.js');
 http.createServer(function (request, response) {
 
 	// 发送 HTTP 头部
 	// HTTP 状态值: 200 : OK
 	// 内容类型: text/plain
 	response.writeHead(200, {'Content-Type': 'text/plain'});
-	// 发送响应数据 "Hello World"
-  var json = {
-     'versionCode':10,
-     'versionName':'test'
-  }
-	response.end(JSON.stringify(json));
+	// 发送响应数据
+	var haha = new version();
+	haha.versionCode = 100;
+	haha.versionName = "哈哈";
+	haha.updateInfo = "这次更新";
+	response.end(JSON.stringify(haha));
 }).listen(8808);
 
 // 终端打印如下信息
